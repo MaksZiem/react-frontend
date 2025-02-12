7918a0805ee48c80d44568912f9eac94453da1ab
MaksZiem <mtziembicki@gmail.com>
2025-02-12 23:12:35 +0100

    income ranking

--- src/Statistics/incomes/pages/Incomes.jsx
+++ src/Statistics/incomes/pages/Incomes.jsx
@@ -6,11 +6,14 @@ import Navbar from "../../components/Navbar";
 import LoadingSpinner from "../../../shared/components/UIElements/LoadingSpinner";
 import { LineChart } from "@mui/x-charts/LineChart";
 import "./Incomes.css";
+import { Gauge, gaugeClasses } from "@mui/x-charts/Gauge";
 
 const initialColor = "rgb(117, 148, 215)";
 
 const Incomes = () => {
   const [dishes, setDishes] = useState(null);
+  const [dishesRanking, setDishesRanking] = useState(null);
+  const [totalProfit, setTotalProfit] = useState(null);
   const { isLoading, error, sendRequest, clearError } = useHttpClient();
   const auth = useContext(AuthContext);
   const [selectedCategory, setSelectedCategory] = useState("wszystkie");
@@ -38,7 +41,6 @@ const Incomes = () => {
     fetchCategories();
   }, [sendRequest]);
 
-
   const fetchData = async () => {
     try {
       const responseData = await sendRequest(
@@ -60,11 +62,13 @@ const Incomes = () => {
           "Content-Type": "application/json",
         }
       );
-      
+
       console.log(responseData);
       console.log(responseData2);
-  
-      setDishes(responseData);
+
+      setDishes(responseData.results);
+      setDishesRanking(responseData.ranking);
+      setTotalProfit(responseData.globalTotalProfit);
     } catch (err) {
       console.error(err);
     }
@@ -82,12 +86,10 @@ const Incomes = () => {
     setInputName(event.target.value);
   };
 
-
   if (isLoading) {
     return (
       <div>
         <LoadingSpinner asOverlay />
-        Ładowanie danych...
       </div>
     );
   }
@@ -126,8 +128,71 @@ const Incomes = () => {
     <div className="container-statistics">
       <Navbar />
       <div className="content">
-      <h1 className="text2">Prognozy wyników dań</h1>
-      {/* <div className="search-container">
+        {totalProfit && (
+          <>
+            <h1 className="text5">Prognozowany przychód całkowity</h1>
+            <div className="ranking3">
+              <div className="gauge-item">
+                <div className="gauge-item2">
+                  <Gauge
+                    sx={(theme) => ({
+                      [`& .${gaugeClasses.valueText}`]: {
+                        fontSize: 24,
+                      },
+                      [`& .${gaugeClasses.valueArc}`]: {
+                        fill: "rgb(117, 148, 215)",
+                      },
+                      [`& .${gaugeClasses.referenceArc}`]: {
+                        fill: theme.palette.text.disabled,
+                      },
+                    })}
+                    value={totalProfit.toFixed(2)}
+                    startAngle={0}
+                    endAngle={360}
+                    innerRadius="80%"
+                    outerRadius="100%"
+                  />
+                </div>
+                <span>Przychód</span>
+              </div>
+            </div>
+          </>
+        )}
+
+        {dishesRanking && !isLoading && (
+          <>
+            <h1 className="text5">Ranking dań</h1>
+            <div className="list-form-placeholder-ingredient-incomes">
+              <div className="ingredients-list-desc">
+                <span className="item-name-ingredient">lp.</span>
+                <span className="dish-update-name-incomes">nazwa</span>
+                <span className="item-category">przychód całkowity</span>
+              </div>
+            </div>
+
+            <ul className="dish-update-list-form-incomes">
+              {dishesRanking.map((dish, index) => (
+                <li
+                  key={dish._id}
+                  className={
+                    index === dishes.length - 1
+                      ? "last-ingredient2"
+                      : "cart-item-ingredient-incomes"
+                  }
+                >
+                  <span className="dish-update-name">{index + 1}</span>
+                  <span className="dish-update-name-incomes">
+                    {dish.dishName}
+                  </span>
+                  <span className="dish-update-category">
+                    {dish.totalProfit.toFixed(2)} PLN
+                  </span>
+                </li>
+              ))}
+            </ul>
+          </>
+        )}
+        {/* <div className="search-container">
         <form className="search-forms">
           <div className="select-category">
             <label htmlFor="name">Nazwa:</label>
@@ -156,127 +221,135 @@ const Incomes = () => {
           </div>
         </form>
       </div> */}
+      {dishes && (
+        <h1 className="text5">Statystyki poszczególnych dań</h1>
+      )}
         {dishes &&
+          !isLoading &&
           dishes.map((dish, index) => (
-            <div key={index} className="dish-info2">
-              <div className="dish-info-content-incomes">
-                <h1>{dish.dishName}</h1>
-                <p>Cena Dania: {dish.dishPrice} zł</p>
-                <p>Marża: {dish.profitMargin.toFixed(2)}%</p>
-                <p>Wartość Marży: {dish.marginValue.toFixed(2)} zł</p>
-                <p>
-                  Łączny Przychód: {dish.totalProjectedRevenue.toFixed(2)} zł
-                </p>
+            <>
+              <div key={index} className="dish-info2">
+                <div className="dish-info-content-incomes">
+                  <h1>{dish.dishName}</h1>
+                  <p>Cena Dania: {dish.dishPrice} zł</p>
+                  <p>Marża: {dish.profitMargin.toFixed(2)}%</p>
+                  <p>Wartość Marży: {dish.marginValue.toFixed(2)} zł</p>
+                  <p>
+                    Łączny Przychód: {dish.totalProjectedRevenue.toFixed(2)} zł
+                  </p>
 
-                <h2>Szczegóły Składników</h2>
+                  <h2>Szczegóły Składników</h2>
 
-                <div className="place-list-form-placeholder-ingredient">
-                  <div className="cook-list-desc">
-                    <div className="cook-name">Nazwa Składnika</div>
-                    <span className="cook-name">Waga (g)</span>
-                    <span className="cook-surname">Średnia Cena za 1g</span>
-                    <span className="cook-pesel">Koszt Składnika (zł)</span>
-                    <span className="cook-action">Udział w Cenie (%)</span>
+                  <div className="place-list-form-placeholder-ingredient">
+                    <div className="cook-list-desc">
+                      <div className="cook-name">Nazwa Składnika</div>
+                      <span className="cook-name">Waga (g)</span>
+                      <span className="cook-surname">Średnia Cena za 1g</span>
+                      <span className="cook-pesel">Koszt Składnika (zł)</span>
+                      <span className="cook-action">Udział w Cenie (%)</span>
+                    </div>
                   </div>
-                </div>
 
-                <ul className="list-form-cooks">
-                  {dish.ingredientDetails.map((ingredient, i) => (
-                    <li
-                      key={ingredient.id}
-                      className={
-                        index === ingredient.length - 1
-                          ? "last-ingredient"
-                          : "cook-list-item"
-                      }
-                    >
-                      <span className="cook-name">{ingredient.name}</span>
+                  <ul className="list-form-cooks">
+                    {dish.ingredientDetails.map((ingredient, i) => (
+                      <li
+                        key={ingredient.id}
+                        className={
+                          index === ingredient.length - 1
+                            ? "last-ingredient"
+                            : "cook-list-item"
+                        }
+                      >
+                        <span className="cook-name">{ingredient.name}</span>
+                        <span className="cook-surname">
+                          {ingredient.weightInDish}
+                        </span>
+                        <span className="cook-pesel">
+                          {ingredient.averagePriceRatio.toFixed(2)}
+                        </span>
+                        <span className="cook-pesel">
+                          {ingredient.finalCost.toFixed(2)}
+                        </span>
+                        <span className="cook-pesel">
+                          {ingredient.contributionPercentage.toFixed(2)}
+                        </span>
+                      </li>
+                    ))}
+                  </ul>
+
+                  <h2>Prognozowane Zamówienia i Przychody</h2>
+                  <div className="place-list-form-placeholder-ingredient">
+                    <div className="cook-list-desc">
+                      <span className="cook-name">Okres</span>
                       <span className="cook-surname">
-                        {ingredient.weightInDish}
+                        Prognozowane Zamówienia
                       </span>
                       <span className="cook-pesel">
-                        {ingredient.averagePriceRatio.toFixed(2)}
+                        Prognozowany Przychód (zł)
                       </span>
-                      <span className="cook-pesel">
-                        {ingredient.finalCost.toFixed(2)}
-                      </span>
-                      <span className="cook-pesel">
-                        {ingredient.contributionPercentage.toFixed(2)}
-                      </span>
-                    </li>
-                  ))}
-                </ul>
-
-                <h2>Prognozowane Zamówienia i Przychody</h2>
-                <div className="place-list-form-placeholder-ingredient">
-                  <div className="cook-list-desc">
-                    <span className="cook-name">Okres</span>
-                    <span className="cook-surname">Prognozowane Zamówienia</span>
-                    <span className="cook-pesel">Prognozowany Przychód (zł)</span>
+                    </div>
                   </div>
-                </div>
-                <ul className="list-form-cooks">
-                  {dish.projectedOrders.map((orders, i) => (
-                    <li
-                      key={orders.id}
-                      className={
-                        index === orders.length - 1
-                          ? "last-ingredient"
-                          : "cook-list-item"
-                      }
-                    >
-                      <span className="cook-surname">
-                      {getMonthNames(dish.projectedOrders.length)[i]}
-                      </span>
-                      <span className="cook-pesel">
-                        {orders.toFixed(2)}
-                      </span>
-                      <span className="cook-pesel">
-                      {dish.projectedRevenues[i].toFixed(2)}
-                      </span>
-                    </li>
-                  ))}
-                </ul>
+                  <ul className="list-form-cooks">
+                    {dish.projectedOrders.map((orders, i) => (
+                      <li
+                        key={orders.id}
+                        className={
+                          index === orders.length - 1
+                            ? "last-ingredient"
+                            : "cook-list-item"
+                        }
+                      >
+                        <span className="cook-surname">
+                          {getMonthNames(dish.projectedOrders.length)[i]}
+                        </span>
+                        <span className="cook-pesel">{orders.toFixed(2)}</span>
+                        <span className="cook-pesel">
+                          {dish.projectedRevenues[i].toFixed(2)}
+                        </span>
+                      </li>
+                    ))}
+                  </ul>
 
-                <h3>Prognoza Przychodów (Wykres)</h3>
-                <div className="center">
-                  {dishes && (
-                    <LineChart
-                      xAxis={[
-                        {
-                          scaleType: "band",
-                          data: getMonthNames(dish.projectedOrders.length), // Month names
-                          legend: {
-                            text: {
-                              fill: "white",
+                  <h3>Prognoza Przychodów (Wykres)</h3>
+                  <div className="center">
+                    {dishes && (
+                      <LineChart
+                        xAxis={[
+                          {
+                            scaleType: "band",
+                            data: getMonthNames(dish.projectedOrders.length), // Month names
+                            legend: {
+                              text: {
+                                fill: "white",
+                              },
+                            },
+                            ticks: {
+                              line: {
+                                stroke: "white",
+                                strokeWidth: 1,
+                              },
+                              text: {
+                                fill: "white",
+                              },
                             },
                           },
-                          ticks: {
-                            line: {
-                              stroke: "white",
-                              strokeWidth: 1,
-                            },
-                            text: {
-                              fill: "white",
-                            },
+                        ]}
+                        series={[
+                          {
+                            data: dish.projectedRevenues,
+                            color: initialColor,
+                            area: true,
                           },
-                        },
-                      ]}
-                      series={[
-                        {
-                          data: dish.projectedRevenues,
-                          color: initialColor,
-                          area: true,
-                        },
-                      ]}
-                      colors={{ scheme: "nivo" }}
-                      width={600}
-                      height={300}
-                    />
-                  )}
+                        ]}
+                        colors={{ scheme: "nivo" }}
+                        width={600}
+                        height={300}
+                      />
+                    )}
+                  </div>
                 </div>
               </div>
-            </div>
+            </>
           ))}
       </div>
     </div>
